1a7914a8ce242938bbbb07b4e88afe62a1e250a7
João Fernandes <joaofnds@joaofnds.com>
2022-10-13 13:14:11 -0300

    [fleet] add livecheck

--- Casks/fleet.rb
+++ Casks/fleet.rb
@@ -6,6 +6,13 @@ cask "fleet" do
   desc "Next-generation IDE by JetBrains"
   homepage "https://www.jetbrains.com/fleet/"
 
+  livecheck do
+    url "https://data.services.jetbrains.com/products?code=FL&latest=true&type=preview"
+    strategy :page_match do |page|
+      JSON.parse(page)[0]["releases"][0]["version"]
+    end
+  end
+
   on_intel do
     sha256 "11f6b4eb6e1e58134553afb9681734d74618ae9e3efc861b03eae4260506af4d"
     url "https://download-cdn.jetbrains.com/fleet/installers/macos_x64/Fleet-#{version}.dmg"
